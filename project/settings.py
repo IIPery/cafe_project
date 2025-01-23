a2390ebcc8e1caf5ad74a6591e4b98aa15bb2036
iskander <pery3333@gmail.com>
2025-01-23 23:18:17 +0500

    Правки

--- project/settings.py
+++ project/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '_')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 
